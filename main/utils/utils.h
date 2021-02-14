35fcbe38940049aab53e2b4f45c6c266b7eb05d5
innc11 <pm@innc11.cn>
2021-02-14 17:35:10 +0800

    refactor: 将map_v函数改为内联

--- main/utils/utils.h
+++ main/utils/utils.h
@@ -10,4 +10,7 @@
         } \
     }while(0)
 
-float map_v(float val, float I_Min, float I_Max, float O_Min, float O_Max);
+inline float map_v(float val, float I_Min, float I_Max, float O_Min, float O_Max)
+{
+	return (((val-I_Min)*((O_Max-O_Min)/(I_Max-I_Min)))+O_Min);
+}
